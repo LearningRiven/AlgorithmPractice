4aad601eed6265f7bd45f8d01720aef3c0a31d49
LearningRiven <87784388+LearningRiven@users.noreply.github.com>
2025-08-03 16:48:32 -0500

    Added tests and skeleton for selection sort

--- src/main/java/org/algomonster/algorithms/sort/SelectionSort.java
+++ src/main/java/org/algomonster/algorithms/sort/SelectionSort.java
@@ -1,4 +1,16 @@
 package org.algomonster.algorithms.sort;
 
+
+import org.algomonster.Utils;
+
+import java.util.List;
+
 public class SelectionSort {
+    public static void sortListSelection(List<Integer> unsortedList){
+        if(unsortedList == null){
+            throw new IllegalStateException("List is null");
+        }
+
+        //Code Goes here
+    }
 }

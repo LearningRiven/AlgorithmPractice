db57ae0362415e3ed2855ad81456996151e0a2d1
LearningRiven <87784388+LearningRiven@users.noreply.github.com>
2025-08-03 17:24:33 -0500

    Finished selection sort

--- src/main/java/org/algomonster/algorithms/sort/SelectionSort.java
+++ src/main/java/org/algomonster/algorithms/sort/SelectionSort.java
@@ -11,6 +11,22 @@ public class SelectionSort {
             throw new IllegalStateException("List is null");
         }
 
-        //Code Goes here
+        for(int j = 0; j < unsortedList.size(); j++){
+            //Current position is assumed to be the minimum
+            int min = j;
+
+            //Find the actual minimum, starting from the index
+            for(int k = j; k < unsortedList.size(); k++){
+                //looks like K is actually the minimum, update the index
+                if(Utils.compare(unsortedList.get(k),unsortedList.get(min)) < 0){
+                    min = k;
+                }
+            }
+
+            //now that we have the minimum, we swap, current j goes to min spot, found min goes to j spot
+            Integer minElement = unsortedList.get(min); //pull min element out
+            unsortedList.set(min,unsortedList.get(j));
+            unsortedList.set(j,minElement);
+        }
     }
 }

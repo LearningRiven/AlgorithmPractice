e1494f54006667d4afbbfb29ce9874b395405869
LearningRiven <87784388+LearningRiven@users.noreply.github.com>
2025-08-06 16:12:21 -0500

    Fixed Utils test, using equals instead of true

--- src/test/java/org/algomonster/UtilsTest.java
+++ src/test/java/org/algomonster/UtilsTest.java
@@ -8,19 +8,19 @@ public class UtilsTest {
     @Test
     void testCompare(){
         //String
-        assertTrue(Utils.compare("Apple","Apple") == 0);
+        assertEquals(0,Utils.compare("Apple","Apple"));
         assertTrue(Utils.compare("Orange","Apple") > 0);
         assertTrue(Utils.compare("Apple","Orange") < 0);
         assertTrue(Utils.compare(null,"Orange") < 0);
         assertTrue(Utils.compare("Orange",null) > 0);
-        assertTrue(Utils.compare(null,null) == 0);
+        assertEquals(0,Utils.compare(null,null));
 
         //Integer
-        assertTrue(Utils.compare(1,1) == 0);
+        assertEquals(0,Utils.compare(1,1));
         assertTrue(Utils.compare(10,1) > 0);
         assertTrue(Utils.compare(1,10) < 0);
         assertTrue(Utils.compare(null,10) < 0);
         assertTrue(Utils.compare(10,null) > 0);
-        assertTrue(Utils.compare(null,null) == 0);
+        assertEquals(0,Utils.compare(null,null));
     }
 }

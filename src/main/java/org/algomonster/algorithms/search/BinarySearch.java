cb8f91573b65391a4ce8bfd9a2db6b3635c30497
LearningRiven <87784388+LearningRiven@users.noreply.github.com>
2025-08-16 15:39:02 -0500

    Finished binary search with testing

--- src/main/java/org/algomonster/algorithms/search/BinarySearch.java
+++ src/main/java/org/algomonster/algorithms/search/BinarySearch.java
@@ -12,13 +12,32 @@ public class BinarySearch {
 
     public static boolean searchListBinary(List<Integer> sortedList, Integer value){
         if(sortedList == null){
-            throw new IllegalStateException("List is null");
+            throw new IllegalStateException("List is null or empty");
         }
 
         boolean found = false;
 
-        //Logic goes here
+        //Boundaries of our array to be resized
+        int start = 0;
+        int end = sortedList.size()-1;
+
+        while(start <= end){
+            //Check the middle slot
+            int middle = Utils.calculateMiddle(start,end);
+            if(Utils.compare(value, sortedList.get(middle)) == 0){ //we found it
+                found = true;
+                break;
+            }
+            else if(Utils.compare(value,sortedList.get(middle)) < 0){ //value is less
+                end = middle - 1; //drop the right side
+            }
+            else{ //its bigger
+                start = middle + 1; //drop the left side
+            }
+        }
 
         return found;
     }
+
+
 }
--- src/test/java/org/algomonster/algorithms/search/BinarySearchTest.java
+++ src/test/java/org/algomonster/algorithms/search/BinarySearchTest.java
@@ -21,6 +21,12 @@ public class BinarySearchTest {
     }
 
     @Test
+    void testBasicExample(){
+        List<Integer> ordered = Arrays.asList(1,3,6,8,9,10);
+        assertTrue(BinarySearch.searchListBinary(ordered,8));
+    }
+
+    @Test
     void testRegularSearchEven(){
         List<Integer> ordered = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
         assertTrue(BinarySearch.searchListBinary(ordered,7));
@@ -77,7 +83,9 @@ public class BinarySearchTest {
 
     @Test
     void testNull(){
-        assertThrows(IllegalStateException.class,() -> BinarySearch.searchListBinary(null,5), "List is null");
-        assertThrows(IllegalStateException.class,() -> BinarySearch.searchListBinary(null,null), "List is null");
+        List<Integer> ordered = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+        assertThrows(IllegalStateException.class,() -> BinarySearch.searchListBinary(null,5), "List is null or empty");
+        assertThrows(IllegalStateException.class,() -> BinarySearch.searchListBinary(null,null), "List is null or empty");
+        assertFalse(BinarySearch.searchListBinary(ordered,null));
     }
 }
